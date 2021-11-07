73f707c12f205530a8cc8128dc5c219657c1ff9c
nadiapla <nadiapla7@gmail.com>
2021-11-07 16:45:51 +0900

    first commit. Add Permission Activity

---
+++ app/src/main/java/com/example/newernews/network/NetworkModule.kt
@@ -0,0 +1,27 @@
+/*
+package com.example.newernews.network
+
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object NetworkModule {
+
+    private val httpClient by lazy {
+        OkHttpClient.Builder()
+            .addNetworkInterceptor(HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY))
+            .build()
+    }
+
+    */
+/*@Singleton
+    @Provides
+    fun get*//*
+
+}*/
